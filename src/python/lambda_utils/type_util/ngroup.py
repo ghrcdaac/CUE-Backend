734e21fd0a45b8de505c73cf27d376c12784e0c9
Navaneeth R Selvaraj <navaneeth026@gmail.com>
2025-01-16 16:25:48 -0600

    adding ngroup func

---
+++ src/python/lambda_utils/type_util/ngroup.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, UUID4
+from typing import Tuple, Optional
+from uuid import UUID
+
+class NgroupCreate(BaseModel):
+    id: Optional[UUID4] = None
+    short_name: str
+    long_name: str
+
+class NgroupReturn(NgroupCreate):
+    id: UUID4
+
+    @classmethod
+    def from_db_row(cls, row: Tuple) -> "NgroupReturn":
+        """Factory function to create an NgroupReturn instance from a database row."""
+        id, short_name, long_name = row
+        return cls(id=id, short_name=short_name, long_name=long_name)
