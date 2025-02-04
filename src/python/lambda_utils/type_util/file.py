73cbf6bf4851e8eac2170e83c76bb1d25d8a4fa1
Navaneeth R Selvaraj <navaneeth026@gmail.com>
2025-02-04 16:25:03 -0600

    Adding CRUD for file

---
+++ src/python/lambda_utils/type_util/file.py
@@ -0,0 +1,51 @@
+from pydantic import BaseModel
+from typing import Optional, Tuple
+from uuid import UUID
+from datetime import datetime
+
+class FileBase(BaseModel):
+    name: str
+    type: str
+    checksum: str
+
+class FileCreate(FileBase):
+    cueuser_uploaded: UUID
+    collection_id: UUID
+    size_bytes: int
+    edpub: Optional[bool] = False
+
+class FileUpdate(FileBase):
+    name: Optional[str] = None
+    type: Optional[str] = None
+    checksum: Optional[str] = None
+    size_bytes: Optional[int] = None
+
+class FileReturn(FileBase):
+    id: UUID
+    cueuser_uploaded: UUID
+    collection_id: UUID    
+    size_bytes: int
+    edpub: bool
+
+    @classmethod
+    def from_db_row(cls, row: Tuple) -> "FileReturn":
+        (
+            id, 
+            name, 
+            file_type, 
+            cueuser_uploaded, 
+            size_bytes, 
+            collection_id, 
+            edpub, 
+            checksum
+        ) = row  # Unpack the tuple correctly
+        return cls(
+            id=id, 
+            name=name, 
+            type=file_type, 
+            cueuser_uploaded=cueuser_uploaded, 
+            size_bytes=size_bytes, 
+            collection_id=collection_id, 
+            edpub=edpub, 
+            checksum=checksum
+        )
