daec479ed7260f230ec7b0f00a5da63d687aac60
Jerrold Williams <williamsjr@ornl.gov>
2025-07-28 08:22:53 -0400

    Remove cue_archive_bucket from main.tf now in glue/s3.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,3 @@ resource "aws_ssm_parameter" "metric_retention_period" {
     type  = "String"
     value = var.metric_retention_period_value
 }
-
-resource "aws_s3_bucket" "cue_archive_bucket"{
-    bucket = var.cue_archive_bucket
-}
