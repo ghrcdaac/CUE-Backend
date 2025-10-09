22bbc3079cee57df4fb427120606644557f3d419
Jerrold Williams <williamsjr@ornl.gov>
2025-10-09 10:24:42 -0400

    Remove logging setup

--- src/python/api/v2/database_util/archive.py
+++ src/python/api/v2/database_util/archive.py
@@ -4,9 +4,6 @@ from asyncpg import Connection
 from typing import List, Dict, Any
 from uuid import UUID
 from .file_metrics import _build_metrics_query_parts # Re-use the powerful query builder
-import structlog
-
-logger = structlog.get_logger(__name__)
 
 async def get_file_ids_by_filter(conn: Connection, ngroup_id: UUID, filters: Dict[str, Any]) -> List[UUID]:
     """
